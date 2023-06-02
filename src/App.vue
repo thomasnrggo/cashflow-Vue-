c315ca2973c84279b38cfba735041dafd3dc54d0
Anthony Gonzalez <thomasnrggo@gmail.com>
2023-06-02 08:13:44 -0600

    add splashScreen with suspense component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <HomePage/>
+  <Suspense>
+    <template #default>
+      <HomePage/>
+    </template>
+    <template #fallback>
+      <SplashScreen/>
+    </template>
+  </Suspense>
 </template>
 
 <script>
-import HomePage from '@/components/HomePage.vue';
+import { defineAsyncComponent } from 'vue'
+import SplashScreen from '@/components/SplashScreen.vue'
 
 export default {
   name: 'App',
   components: {
-    HomePage
+    HomePage: defineAsyncComponent(() => 
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(import('@/components/HomePage.vue'))
+        }, 2000);
+      })
+    ),
+    SplashScreen
 }
 }
 </script>
 
 <style>
+html,
+body,
+.app {
+  min-height: 100vh;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+* {
+  --brand-green: #04b500;
+  --brand-blue: #0689b0;
+}
 </style>
