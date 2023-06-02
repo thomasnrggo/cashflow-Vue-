ccf640b91e3024d00dca00b32ddf0430878e521c
Anthony Gonzalez <thomasnrggo@gmail.com>
2023-06-02 09:31:06 -0600

    complete layout

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,3 +1,30 @@
 <template>
-    <div>Home page</div>
+    <Layout>
+        <template #header>
+            <Header></Header>
+        </template>
+        <template #resume>
+            <Resume />
+        </template>
+        <template #movements>
+            <Movements />
+        </template>
+    </Layout>
 </template>
+
+<script>
+import LayoutComponent from './LayoutComponent'
+import HeaderComponent from './HeaderComponent'
+import Resume from './resume/index.vue'
+import MovementsComponent from './MovementsComponent'
+
+
+export default {
+    components: {
+        Layout: LayoutComponent,
+        Header: HeaderComponent,
+        Resume,
+        Movements: MovementsComponent
+    }
+}
+</script>
