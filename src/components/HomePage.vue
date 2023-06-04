e25f9d7e3567abf8ffccc4213fd54c0a50aa9524
Anthony Gonzalez <thomasnrggo@gmail.com>
2023-06-04 12:46:15 -0600

    add history component

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,10 +4,21 @@
             <Header></Header>
         </template>
         <template #resume>
-            <Resume />
+            <Resume 
+                :label="'Total savings'"
+                :total-amount="150000"
+                :amount="amount"
+            >
+                <template #graphic>
+                    graphic
+                </template>
+                 <template  #action>
+                    action
+                </template>
+            </Resume>
         </template>
         <template #movements>
-            <Movements />
+            <Movements :movements="movements"/>
         </template>
     </Layout>
 </template>
@@ -16,7 +27,7 @@
 import LayoutComponent from './LayoutComponent'
 import HeaderComponent from './HeaderComponent'
 import Resume from './resume/index.vue'
-import MovementsComponent from './MovementsComponent'
+import Movements from './Movements/index.vue'
 
 
 export default {
@@ -24,7 +35,44 @@ export default {
         Layout: LayoutComponent,
         Header: HeaderComponent,
         Resume,
-        Movements: MovementsComponent
+        Movements
+    },
+    data() {
+        return {
+            amount: null,
+            movements: [
+                {
+                    id: 1,
+                    title: "Movement",
+                    description: "Deposit of salary",
+                    amount: "1000",
+                },
+                {
+                    id: 2,
+                    title: "Movement 1",
+                    description: "Deposit of UTA",
+                    amount: "500",
+                },
+                {
+                    id: 3,
+                    title: "Movement 3",
+                    description: "Food",
+                    amount: "-100",
+                },
+                {
+                    id: 4,
+                    title: "Movement 4",
+                    description: "Collage",
+                    amount: "1000",
+                },
+                {
+                    id: 5,
+                    title: "Movement 5",
+                    description: "Fix laptop",
+                    amount: "1000",
+                },
+            ]
+        }
     }
 }
 </script>
--- src/components/resume/index.vue
+++ src/components/resume/index.vue
@@ -1,4 +1,73 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <main class="resume">resume</main>
+    <main class="resume">
+        <p>{{ label }}</p>
+        <h1>{{ currency }}</h1>
+        <div class="graphic">
+            <slot name='graphic'></slot>
+        </div>
+        <div class="action">
+            <slot name="action"></slot>
+        </div>
+    </main>
 </template>
+
+<script>
+const currencyFormatter = new Intl.NumberFormat('es-MX', {
+    style: 'currency',
+    currency: 'MXM',
+});
+
+export default {
+    props: {
+        label: {
+            type: String
+        },
+        totalAmount: {
+            type: Number
+        },
+        amount: {
+            type: Number,
+            default: null
+        }
+    },
+    computed: {
+        amountToDisplay() {
+            return this.amount !== null ? this.amount : this.totalAmount
+        },
+        currency() {
+            
+            return currencyFormatter.format(this.amountToDisplay)
+        }
+    }
+}
+</script>
+
+<style scoped>
+main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+}
+
+h1,
+p {
+    margin: 0;
+    text-align: center;
+}
+
+h1 {
+    margin-top: 14px;
+    color: var(--brand-green);
+}
+
+.graphic {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 48px 24px;
+    box-sizing: border-box;
+}</style>
